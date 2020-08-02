64b95b239987a07366d6082a261b133f8959b9e2
Mohd Ashraf <mohdashraf010897@gmail.com>
2020-08-03 01:16:44 +0530

    Updates

---
+++ precache-manifest.90dbff8270f543e6f6a58da3423b74bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7e406eb496510794a4362caf7cf15a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4305b678c32609722b7",
+    "url": "/static/css/2.a0babc62.chunk.css"
+  },
+  {
+    "revision": "dc62bf610a4be48c484b",
+    "url": "/static/css/main.322c7bfb.chunk.css"
+  },
+  {
+    "revision": "e4305b678c32609722b7",
+    "url": "/static/js/2.e45a053e.chunk.js"
+  },
+  {
+    "revision": "3ae53551bb61526a017fad7619b64b1b",
+    "url": "/static/js/2.e45a053e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc62bf610a4be48c484b",
+    "url": "/static/js/main.5e4553f0.chunk.js"
+  },
+  {
+    "revision": "21404b212f3b3c74d5aa",
+    "url": "/static/js/runtime-main.9150da7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
